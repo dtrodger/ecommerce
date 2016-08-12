dd76c94c74dbefe721f0c2e44742841e80a5ab14
David Rodgers <dtrodger20@gmail.com>
2016-08-12 15:18:20 -0500

    adds unique slug generation pre save

--- src/digitalmarket/mixins.py
+++ src/digitalmarket/mixins.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 
 
-class MultiSlugMixin(object):
-	def get_object(self, *args, **kwargs):
-		model = None
-		slug = self.kwargs.get("slug")
-		ModelClass = self.model
+# class MultiSlugMixin(object):
+# 	def get_object(self, *args, **kwargs):
+# 		model = None
+# 		slug = self.kwargs.get("slug")
+# 		ModelClass = self.model
 		
-		if slug is not None:
-			try:
-				obj = get_object_or_404(ModelClass, slug=slug)
-			except ModelClass.MultipleObjectsReturned:
-				obj = ModelClass.objects.filter(slug=slug).order_by("-title").first()
-		else:
-			obj = super(MultiSlugMixin, self).get_object(*args, **kwargs)
-		return obj
+# 		if slug is not None:
+# 			try:
+# 				obj = get_object_or_404(ModelClass, slug=slug)
+# 			except ModelClass.MultipleObjectsReturned:
+# 				obj = ModelClass.objects.filter(slug=slug).order_by("-title").first()
+# 		else:
+# 			obj = super(MultiSlugMixin, self).get_object(*args, **kwargs)
+# 		return obj
 
 
 
--- src/products/forms.py
+++ src/products/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.text import slugify
 
 from .models import Product
 
@@ -7,38 +8,6 @@ PUBLISH_CHOICES = (
 	('publish', 'Publish'),
 	('draft', 'Draft'),
 	)
-
-class ProductForm(forms.Form):
-	title = forms.CharField(label="Your Title", widget=forms.TextInput(
-			attrs={
-				"class": "custom-class",
-				"placeholder": "Title",
-			}
-	))
-	description = forms.CharField(widget=forms.Textarea(
-			attrs={
-				"class": "custom-class",
-				"placeholder": "Description",
-			}
-	))
-	price = forms.DecimalField()
-	publish = forms.ChoiceField(widget=forms.RadioSelect, choices=PUBLISH_CHOICES, required=False)
-
-	def clean_price(self):
-		price = self.cleaned_data.get("price")
-		if price <= 1.00:
-			raise forms.ValidationError("Price must be greater than 1")
-		elif price >= 100:
-			raise forms.ValidationError("Price must be less than 100")
-		else:
-			return price
-
-	def clean_title(self):
-		title = self.cleaned_data.get("title")
-		if len(title) > 3:
-			return title
-		else:
-			raise forms.ValidationError("Title must be grater than 3 characters long")
 
 
 class ProductModelForm(forms.ModelForm):
@@ -65,6 +34,17 @@ class ProductModelForm(forms.ModelForm):
 				})
 		}
 
+	# def clean(self, *args, **kwargs):
+	# 	cleaned_data = super(ProductModelForm, self).clean(*args, **kwargs)
+	# 	title = cleaned_data.get("title")
+	# 	slug = slugify(title)
+	# 	qs = Product.objects.filter(slug=slug).exists()
+
+	# 	if qs:
+	# 		raise forms.ValidationError("This title is taken. Please choose a different title.")
+
+	# 	return cleaned_data
+
 	def clean_price(self):
 		price = self.cleaned_data.get("price")
 		if price <= 1.00:
@@ -82,5 +62,34 @@ class ProductModelForm(forms.ModelForm):
 			raise forms.ValidationError("Title must be grater than 3 characters long")
 
 
+# class ProductForm(forms.Form):
+# 	title = forms.CharField(label="Your Title", widget=forms.TextInput(
+# 			attrs={
+# 				"class": "custom-class",
+# 				"placeholder": "Title",
+# 			}
+# 	))
+# 	description = forms.CharField(widget=forms.Textarea(
+# 			attrs={
+# 				"class": "custom-class",
+# 				"placeholder": "Description",
+# 			}
+# 	))
+# 	price = forms.DecimalField()
+# 	publish = forms.ChoiceField(widget=forms.RadioSelect, choices=PUBLISH_CHOICES, required=False)
 
+# 	def clean_price(self):
+# 		price = self.cleaned_data.get("price")
+# 		if price <= 1.00:
+# 			raise forms.ValidationError("Price must be greater than 1")
+# 		elif price >= 100:
+# 			raise forms.ValidationError("Price must be less than 100")
+# 		else:
+# 			return price
 
+# 	def clean_title(self):
+# 		title = self.cleaned_data.get("title")
+# 		if len(title) > 3:
+# 			return title
+# 		else:
+# 			raise forms.ValidationError("Title must be grater than 3 characters long")
