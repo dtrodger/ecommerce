5f932a92460ac3e4ccc9e2aeb4cee01e31c4aad4
David Rodgers <dtrodger20@gmail.com>
2016-08-12 12:43:46 -0500

    adds form and product create view

--- src/products/views.py
+++ src/products/views.py
@@ -2,7 +2,22 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 
 from .models import Product
+from .forms import ProductCreateForm
 # Create your views here.
+
+def create_view(request):
+	form = ProductCreateForm(request.POST or None)
+	if form.is_valid():
+		data = form.cleaned_data
+		title = data.get("title")
+		description = data.get("description")
+		price = data.get("price")
+		new_obj = Product.objects.create(title=title, description=description, price=price)
+	template = "create_view.html"
+	context = {
+		"form" : form
+	}
+	return render(request, template, context)
 
 def detail_slug_view(request, slug=None):
 	try:
