bfee834c5c2b92ac61e3d596d9147a773da04658
David Rodgers <dtrodger20@gmail.com>
2016-08-18 23:00:24 -0500

    tabular inline admin

--- src/products/admin.py
+++ src/products/admin.py
@@ -2,13 +2,19 @@ from django.contrib import admin
 
 from .models import Product, MyProducts, Thumbnail
 
+class ThumbnailInLine(admin.TabularInline):
+	extra = 1
+	model = Thumbnail
+
 class ProductAdmin(admin.ModelAdmin):
+	inlines = [ThumbnailInLine]
 	list_display = ["__unicode__", "description", "price", "sale_price"]
 	search_fields = ["title", "description"]
 	list_filter = ["price", "sale_price"]
 	list_editable = ["sale_price"]
 	class Meta:
 		model = Product
+
 
 admin.site.register(Product, ProductAdmin)
 
