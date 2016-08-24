28751b79cf1eb93149e067ed1bdd655f47299719
David Rodgers <dtrodger20@gmail.com>
2016-08-23 22:55:18 -0500

    adds thumbnail filter customer template tag

---
+++ src/products/templatetags/get_thumbnail.py
@@ -0,0 +1,15 @@
+from django import template
+
+register = template.Library()
+
+from ..models import Product, THUMB_CHOICES
+
+@register.filter
+def get_thumbnail(obj, arg):
+	if not isinstance(obj, Product):
+		raise TypeError("This is not a valid product model.")
+	arg = arg.lower()
+	choices = dict(THUMB_CHOICES)
+	if not choices.get(arg):
+		raise TypeError("This is not a valid type for this model.")
+	return obj.thumbnail_set.filter(type=arg).first().media.url
