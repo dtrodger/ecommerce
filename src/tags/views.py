3e81e34bcfb9220b20689759e4f5911649cec9d4
David Rodgers <dtrodger20@gmail.com>
2016-08-24 22:04:34 -0500

    refactor analytic addiction

--- src/tags/views.py
+++ src/tags/views.py
@@ -14,12 +14,8 @@ class TagDetailView(DetailView):
 		context = super(TagDetailView, self).get_context_data(*args, **kwargs)
 		print self.get_object().products.all()
 		if self.request.user.is_authenticated():
-			analytic_obj, created = TagView.objects.get_or_create(
-					user=self.request.user,
-					tag=self.get_object()
-					)
-			analytic_obj.count += 1
-			analytic_obj.save()
+			tag = self.get_object()
+			new_view = TagView.objects.add_count(self.request.user, tag)
 		return context
 
 class TagListView(ListView):
